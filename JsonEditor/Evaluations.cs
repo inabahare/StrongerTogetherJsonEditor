4fdd7ce9ac9c7dea7131ccdc592ec0c3b34ff6d3
inaba <software@panduro.guru>
2019-10-08 21:56:30 +0200

    init

---
+++ JsonEditor/Evaluations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonEditor
+{
+    class Evaluations
+    {
+        public List<Response> Responses { get; set; }
+    }
+
+    class Response
+    {
+        public string Title { get; set; }
+        public string Theme { get; set; }
+        public List<Responses> Responses { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Theme}";
+        }
+    }
+
+    class Responses
+    {
+        public string Name { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ StrongerTogetherJsonEditor/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace StrongerTogetherJsonEditor
+{
+    public class Class1
+    {
+    }
+}
