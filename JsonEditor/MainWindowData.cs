f6f2f877119f9050ebd166dff0e74d07b4600ce0
inaba <software@panduro.guru>
2019-10-23 10:44:16 +0200

    More code removalgit add .

--- JsonEditor/MainWindowData.cs
+++ JsonEditor/MainWindowData.cs
@@ -11,27 +11,11 @@ namespace JsonEditor
         ObservableCollection<Scenario> Scenarios { get; set; }
         ObservableCollection<Response> Responses { get; set; }
 
-        ObservableCollection<string> _setupData;
-
         Response _selectedResponse;
         Scenario _selectedScene;
 
-        ResponseType _responseTypes;
-        private Questions _selectedQuestios;
+        Questions _selectedQuestios;
 
-
-        public ObservableCollection<string> SetupData
-        {
-            get => _setupData;
-            set
-            {
-                if (_setupData == value)
-                    return;
-
-                _setupData = value;
-                OnPropertyChanged(nameof(SetupData));
-            }
-        }
         public Response SelectedResponse
         {
             get => _selectedResponse;
@@ -61,18 +45,6 @@ namespace JsonEditor
 
                 Sections.UpdateLayout();
                 OnPropertyChanged(nameof(SelectedScene));
-            }
-        }
-
-        public ResponseType ResponseTypes
-        {
-            get => _responseTypes;
-            set
-            {
-                if (_responseTypes == value)
-                    return;
-                _responseTypes = value;
-                OnPropertyChanged(nameof(ResponseTypes));
             }
         }
 
